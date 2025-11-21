34a201926bbe6371e2f772e51571dcc6eb74c1c3
ali. <gnualmalki@gmail.com>
2025-11-21 16:36:18 +0000

    lighter cursor line

--- colors/devel.lua
+++ colors/devel.lua
@@ -11,63 +11,64 @@ local c = {
 	magenta = "#cc00ff",
 	cyan    = "#00ffff",
 	brown   = "#A0522D",
+	crline  = "#1f1f1f"
 }
 
 vim.cmd("hi clear")
 if vim.fn.exists("syntax_on") == 1 then
 	vim.cmd("syntax reset")
 end
-vim.g.colors_name = "devel"
+vim.g.colors_name   = "devel"
 vim.o.termguicolors = true
 
-local groups = {
-	Normal            = { fg = c.white, bg = c.black },
-	Cursor            = { fg = c.white, bg = c.black },
-	CursorLine        = { bg = c.black },
-	LineNr            = { fg = c.brown, bg = c.black },
-	CursorLineNr      = { fg = c.brown, bg = c.black },
-	SignColumn        = { bg = c.black },
-	VertSplit         = { fg = c.gray, bg = c.black },
-	StatusLine        = { fg = c.white, bg = c.black },
-	StatusLineNC      = { fg = c.gray, bg = c.black },
-	Comment           = { fg = c.blue },
-	Visual            = { bg = c.gray },
-	Search            = { fg = c.black, bg = c.brown },
-	IncSearch         = { fg = c.black, bg = c.green },
-	Pmenu             = { fg = c.white, bg = c.black },
-	PmenuSel          = { fg = c.brown, bg = c.black },
-	PmenuSbar         = { bg = c.black },
-	PmenuThumb        = { bg = c.gray },
-	EndOfBuffer       = { fg = c.black },
-	MatchParen        = { bg = c.magenta, fg = c.black, bold = true },
-	Keyword           = { fg = c.brown },
-	Statement         = { fg = c.brown },
-	Conditional       = { fg = c.brown },
-	Repeat            = { fg = c.brown },
-	Label             = { fg = c.brown },
-	Exception         = { fg = c.brown },
-	KeywordFunction   = { fg = c.brown },
-	Type              = { fg = c.green },
-	TypeBuiltin       = { fg = c.green },
-	String            = { fg = c.red },
-	Character         = { fg = c.red },
-	Number            = { fg = c.red },
-	Boolean           = { fg = c.red },
-	Constant          = { fg = c.magenta },
-	ConstantBuiltin   = { fg = c.magenta },
-	Identifier        = { fg = c.white },
-	IdentifierBuiltin = { fg = c.magenta },
-	Function          = { fg = c.white },
-	FunctionBuiltin   = { fg = c.magenta },
-	Operator          = { fg = c.white },
-	SpecialChar       = { fg = c.magenta },
-	Special           = { fg = c.white },
-	PreProc           = { fg = c.magenta },
-	Include           = { fg = c.magenta },
-	Define            = { fg = c.magenta },
-	Macro             = { fg = c.magenta },
-	Todo              = { fg = c.magenta, bold = true },
-	Delimiter         = { fg = c.white },
+local groups        = {
+	Normal                      = { fg = c.white, bg = c.black },
+	Cursor                      = { fg = c.white, bg = c.black },
+	CursorLine                  = { bg = c.crline },
+	LineNr                      = { fg = c.brown, bg = c.black },
+	CursorLineNr                = { fg = c.brown, bg = c.black },
+	SignColumn                  = { bg = c.black },
+	VertSplit                   = { fg = c.gray, bg = c.black },
+	StatusLine                  = { fg = c.white, bg = c.black },
+	StatusLineNC                = { fg = c.gray, bg = c.black },
+	Comment                     = { fg = c.blue },
+	Visual                      = { bg = c.gray },
+	Search                      = { fg = c.black, bg = c.brown },
+	IncSearch                   = { fg = c.black, bg = c.green },
+	Pmenu                       = { fg = c.white, bg = c.black },
+	PmenuSel                    = { fg = c.brown, bg = c.black },
+	PmenuSbar                   = { bg = c.black },
+	PmenuThumb                  = { bg = c.gray },
+	EndOfBuffer                 = { fg = c.black },
+	MatchParen                  = { bg = c.magenta, fg = c.black, bold = true },
+	Keyword                     = { fg = c.brown },
+	Statement                   = { fg = c.brown },
+	Conditional                 = { fg = c.brown },
+	Repeat                      = { fg = c.brown },
+	Label                       = { fg = c.brown },
+	Exception                   = { fg = c.brown },
+	KeywordFunction             = { fg = c.brown },
+	Type                        = { fg = c.green },
+	TypeBuiltin                 = { fg = c.green },
+	String                      = { fg = c.red },
+	Character                   = { fg = c.red },
+	Number                      = { fg = c.red },
+	Boolean                     = { fg = c.red },
+	Constant                    = { fg = c.magenta },
+	ConstantBuiltin             = { fg = c.magenta },
+	Identifier                  = { fg = c.white },
+	IdentifierBuiltin           = { fg = c.magenta },
+	Function                    = { fg = c.white },
+	FunctionBuiltin             = { fg = c.magenta },
+	Operator                    = { fg = c.white },
+	SpecialChar                 = { fg = c.magenta },
+	Special                     = { fg = c.white },
+	PreProc                     = { fg = c.magenta },
+	Include                     = { fg = c.magenta },
+	Define                      = { fg = c.magenta },
+	Macro                       = { fg = c.magenta },
+	Todo                        = { fg = c.magenta, bold = true },
+	Delimiter                   = { fg = c.white },
 
 	CompileModeMessage          = { underline = true },
 	CompileModeMessageRow       = { fg = c.white },
@@ -84,7 +85,7 @@ local groups = {
 }
 
 -- terminal colors (fix: use vim.g)
-local g = vim.g
+local g             = vim.g
 g.terminal_color_0  = c.black
 g.terminal_color_1  = c.red
 g.terminal_color_2  = c.green
